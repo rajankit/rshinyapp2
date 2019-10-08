f780e1152dfed5784716f65086f7b29a6814f670
Ankit <ankit.raj.min14@itbhu.ac.in>
2019-10-09 00:26:26 +0530

    First commit

---
+++ app.R
@@ -0,0 +1,83 @@
+library(shiny)
+library(shinydashboard)
+
+header <- dashboardHeader(
+  title = "My Dashboard",
+  dropdownMenu(type = "messages",
+               messageItem(
+                 from = "Sales Dept",
+                 message = "Sales are steady this month."
+               ),
+               messageItem(
+                 from = "New User",
+                 message = "How do I register?",
+                 icon = icon("question"),
+                 time = "13:45"
+               ),
+               messageItem(
+                 from = "Support",
+                 message = "The new server is ready.",
+                 icon = icon("life-ring"),
+                 time = "2014-12-01"
+               )
+  ),
+  dropdownMenu(type = "notifications",
+               notificationItem(
+                 text = "5 new users today",
+                 icon("users")
+               ),
+               notificationItem(
+                 text = "12 items delivered",
+                 icon("truck"),
+                 status = "success"
+               ),
+               notificationItem(
+                 text = "Server load at 86%",
+                 icon = icon("exclamation-triangle"),
+                 status = "warning"
+               )
+  )
+  )
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    # First tab content
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              box(plotOutput("plot1", height = 250)),
+              
+              box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+              )
+            )
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+    )
+  )
+)
+
+ui <-dashboardPage(header, sidebar, body)
+
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
